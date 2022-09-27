b7f7631038540a458b92aee49a61c4c0dd57fe70
Leon7422 <y.kurduban98@gmail.com>
2022-09-27 22:30:05 +0300

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+refs.buttonStop.setAttribute('disabled', '');
+refs.buttonStart.addEventListener('click', itsShowTime);
+refs.buttonStop.addEventListener('click', stopShowTime);
+let intervalForShowTime;
 
+function itsShowTime() {
+  intervalForShowTime = setInterval(createRandomBackground, 1000);
+  refs.buttonStart.setAttribute('disabled', '');
+  refs.buttonStop.removeAttribute('disabled');
+}
+
+function stopShowTime() {
+  clearInterval(intervalForShowTime);
+  refs.body.style.backgroundColor = '#fafafa';
+  refs.buttonStart.removeAttribute('disabled');
+  refs.buttonStop.setAttribute('disabled', '');
+}
+
+function createRandomBackground() {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
